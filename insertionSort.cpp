2e48b0374827cf55c98158e82a6b55e236e0fc84
Md Obaidur Rahman <mdobaidur0090@gmail.com>
2025-11-09 00:50:19 +0600

    insertion Sort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -8,14 +8,12 @@ void printArray(int arr[], int n) {
 }
 
 void insertionSort(int arr[],int n){
-    for(int i=0;i<n;i++){
-        int key=arr[i];
-        int j=i-1;
-        while (j>=0 && arr[j]>key){
-            arr[j+1]=arr[j];
+    for(int i=0;i<n-1;i++){
+        int j=i+1;
+        while(j>=1 && arr[j]<arr[j-1]){
+            swap(arr[j],arr[j-1]);
             j--;
         }
-        arr[j+1]=key;
     }
     printArray(arr,n);
 }
