1f2c451f67195b411f00eb4a9e671c2e0e164c5f
Md Obaidur Rahman <mdobaidur0090@gmail.com>
2025-11-08 23:58:29 +0600

    insertion sort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,6 +1,38 @@
-#include <istream>
+#include <iostream>
 using namespace std;
-int main(){
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
+void insertionSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int key=arr[i];
+        int j=i-1;
+        while (j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+    printArray(arr,n);
+}
+
+void inputArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    insertionSort(arr,n);
+}
+
+int main(){
+    int n;
+    cout<<"Enter your size:";
+    cin>>n;
+    int arr[n];
+    cout << "Enter your "<< n <<" size of  Array :" ;
+    inputArray(arr,n);
     return 0;
 }
